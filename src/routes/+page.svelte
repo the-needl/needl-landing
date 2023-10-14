7f60718b1a635f35f58b22114b04a5ba34d5abd5
Matteo Prandi <matteo.prandi@live.it>
2023-10-14 15:34:20 +0200

    enhance: waitlist form linked to dedicated google sheet

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,4 @@
+
 <!-- 
 <div class="relative isolate overflow-hidden bg-gray-900 py-16 sm:py-24 lg:py-32">
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
@@ -40,18 +41,37 @@
    -->
 
 
-
 <div class="flex justify-center h-screen bg-gray-900 isolate overflow-hidden relative">
         <div class="m-auto">
             <h1 class="text-6xl font-bold text-white opacity-90 blur-sm sm:text-8xl text-center mb-12">&lt;the-needl&gt;</h1>
             <h2 class="mx-4 md:mx-auto text-2xl font-bold text-white opacity-85 sm:text-4xl text-center">Get notified when we're launching.</h2>
-            <div class="mt-6 mx-5 md:mx-auto flex max-w-md gap-x-4">
-                <label for="email-address" class="sr-only">Email address</label>
-                <input id="email-address" name="email" type="email" autocomplete="email" required class="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white opacity-85 shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" placeholder="Enter your email">
-                <button type="submit" class="flex-none rounded-md bg-purple-100 bg-opacity-85 px-3.5 py-2.5 text-sm font-gray-900 text- shadow-sm hover:bg-gray-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Notify me</button>
-            </div>
+
+                <form on:submit|preventDefault={handleSubmit} class="mt-6 mx-5 md:mx-auto flex max-w-md gap-x-4">
+                    <label for="email-address" class="sr-only">Email address</label>
+                    <input disabled={isSubmitted} bind:value={email} id="email-address" name="email" type="email" autocomplete="email" required class="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 {isSubmitted ? 'text-purple-100/50' : 'text-white'} opacity-85 shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" placeholder="Enter your email">
+                    <button disabled={isSubmitted} id="submit-btn" type="submit" class="flex-none rounded-md {isSubmitted ? 'bg-purple-200/50' : 'bg-purple-100'} bg-opacity-85 px-3.5 py-2.5 text-sm font-gray-90 shadow-sm {isSubmitted ? '' :'hover:bg-purple-200'} focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">{isSubmitted ? "Thank you" : "Notify me"}</button>
+                </form>
+
             <div class="absolute left-1/2 top-0 -z-10 -translate-x-1/2 blur-3xl xl:-top-6" aria-hidden="true">
                 <div class="aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
             </div>
         </div>
 </div>
+
+
+
+<script>
+    let email = '';
+    let isSubmitted = false;
+  
+    const handleSubmit = async () => {
+      isSubmitted = true; // Set isSubmitted to true immediately upon form submission.
+  
+      const response = await fetch(
+        `https://script.google.com/macros/s/AKfycbxb-Am3_2C70wxsCINOGvhvsbje36a3Ly6SmY3TVjMQBtHi2sxIu_8eYyrJZOjPHVcifA/exec?email=${email}`,
+        {
+          method: 'POST',
+        }
+      );
+    };
+  </script>
